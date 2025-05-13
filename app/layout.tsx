e98c9c138b2b29ee569c58deab8fca4fc8607bc5
codesvarun <singhvarun9554@gmail.com>
2025-05-13 23:22:07 +0530

    Added all shadcn components

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
+  title: "Varun Singh",
   description: "This website is a portfolio website of varun and it manages by varun.",
 };
 
